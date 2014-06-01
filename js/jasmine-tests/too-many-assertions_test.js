924ee82dd9ca48cacb311010a3c0c22e06ab33b3
Dominik Jungowski <Dominik@Jungowski.de>
2014-06-01 22:21:07 +0200

    Eigene Datei

--- js/jasmine-tests/too-many-assertions_test.js
+++ js/jasmine-tests/too-many-assertions_test.js
@@ -23,25 +23,4 @@ describe('TooManyAssertions', function() {
         expect(factory.isName('ex1')).toBe(false);
         expect(factory.isName('ex2')).toBe(true);
     });
-
-    // Better example
-//    describe('isName(), setName()', function() {
-//        it('has a default', function() {
-//            expect(factory.isName('default')).toBe(true);
-//            expect(factory.isName('Heinz')).toBe(false);
-//        });
-//
-//        it('can be changed', function() {
-//            factory.setName('Heinz');
-//            expect(factory.isName('default')).toBe(false);
-//            expect(factory.isName('Heinz')).toBe(true);
-//        });
-//
-//        it('is not changed when providing undefined', function() {
-//            factory.setName('Heinz');
-//            factory.setName(undefined);
-//            expect(factory.isName('default')).toBe(false);
-//            expect(factory.isName('Heinz')).toBe(true);
-//        });
-//    });
 });
---
+++ js/jasmine-tests/too-many-assertions_test_better.js
@@ -0,0 +1,28 @@
+describe('TooManyAssertions', function() {
+	var factory = null;
+
+	beforeEach(function() {
+		factory = new IPC.TooManyAssertions();
+	});
+
+	// Better example
+    describe('isName(), setName()', function() {
+        it('has a default', function() {
+            expect(factory.isName('default')).toBe(true);
+            expect(factory.isName('Heinz')).toBe(false);
+        });
+
+        it('can be changed', function() {
+            factory.setName('Heinz');
+            expect(factory.isName('default')).toBe(false);
+            expect(factory.isName('Heinz')).toBe(true);
+        });
+
+        it('is not changed when providing undefined', function() {
+            factory.setName('Heinz');
+            factory.setName(undefined);
+            expect(factory.isName('default')).toBe(false);
+            expect(factory.isName('Heinz')).toBe(true);
+        });
+    });
+});
