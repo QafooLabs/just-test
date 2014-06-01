953e1914faf9b6831af170e221c592074d174214
Dominik Jungowski <Dominik@Jungowski.de>
2014-06-01 12:23:11 +0200

    Whoops. Files were somewhow switched^^

--- jasmine-suts/too-many-assertions.js
+++ jasmine-suts/too-many-assertions.js
@@ -1,47 +1,11 @@
-describe('TooManyAssertions', function() {
-    var factory = null;
+IPC.TooManyAssertions = function() {
+    var __name = 'default';
 
-    beforeEach(function() {
-        factory = new IPC.TooManyAssertions();
-    });
+    this.setName = function(name) {
+        __name = name;
+    };
 
-    // ugly example
-    it('#isName, #setName', function() {
-        expect(factory.isName('default')).toBe(true);
-        expect(factory.isName('ex1')).toBe(false);
-        expect(factory.isName('ex2')).toBe(false);
-        factory.setName('ex1');
-        expect(factory.isName('default')).toBe(false);
-        expect(factory.isName('ex1')).toBe(true);
-        expect(factory.isName('ex2')).toBe(false);
-        factory.setName('ex2');
-        expect(factory.isName('default')).toBe(false);
-        expect(factory.isName('ex1')).toBe(false);
-        expect(factory.isName('ex2')).toBe(true);
-        factory.setName(undefined);
-        expect(factory.isName('default')).toBe(false);
-        expect(factory.isName('ex1')).toBe(false);
-        expect(factory.isName('ex2')).toBe(true);
-    });
-
-    // Better example
-//    describe('isName(), setName()', function() {
-//        it('has a default', function() {
-//            expect(factory.isName('default')).toBe(true);
-//            expect(factory.isName('Heinz')).toBe(false);
-//        });
-//
-//        it('can be changed', function() {
-//            factory.setName('Heinz');
-//            expect(factory.isName('default')).toBe(false);
-//            expect(factory.isName('Heinz')).toBe(true);
-//        });
-//
-//        it('is not changed when providing undefined', function() {
-//            factory.setName('Heinz');
-//            factory.setName(undefined);
-//            expect(factory.isName('default')).toBe(false);
-//            expect(factory.isName('Heinz')).toBe(true);
-//        });
-//    });
-});
+    this.isName = function(name) {
+        return (__name == name);
+    };
+};
--- jasmine-tests/too-many-assertions_test.js
+++ jasmine-tests/too-many-assertions_test.js
@@ -1,11 +1,47 @@
-IPC.TooManyAssertions = function() {
-    var __name = 'default';
+describe('TooManyAssertions', function() {
+    var factory = null;
 
-    this.setName = function(name) {
-        __name = name;
-    };
+    beforeEach(function() {
+        factory = new IPC.TooManyAssertions();
+    });
 
-    this.isName = function(name) {
-        return (__name == name);
-    };
-};
+    // ugly example
+    it('#isName, #setName', function() {
+        expect(factory.isName('default')).toBe(true);
+        expect(factory.isName('ex1')).toBe(false);
+        expect(factory.isName('ex2')).toBe(false);
+        factory.setName('ex1');
+        expect(factory.isName('default')).toBe(false);
+        expect(factory.isName('ex1')).toBe(true);
+        expect(factory.isName('ex2')).toBe(false);
+        factory.setName('ex2');
+        expect(factory.isName('default')).toBe(false);
+        expect(factory.isName('ex1')).toBe(false);
+        expect(factory.isName('ex2')).toBe(true);
+        factory.setName(undefined);
+        expect(factory.isName('default')).toBe(false);
+        expect(factory.isName('ex1')).toBe(false);
+        expect(factory.isName('ex2')).toBe(true);
+    });
+
+    // Better example
+//    describe('isName(), setName()', function() {
+//        it('has a default', function() {
+//            expect(factory.isName('default')).toBe(true);
+//            expect(factory.isName('Heinz')).toBe(false);
+//        });
+//
+//        it('can be changed', function() {
+//            factory.setName('Heinz');
+//            expect(factory.isName('default')).toBe(false);
+//            expect(factory.isName('Heinz')).toBe(true);
+//        });
+//
+//        it('is not changed when providing undefined', function() {
+//            factory.setName('Heinz');
+//            factory.setName(undefined);
+//            expect(factory.isName('default')).toBe(false);
+//            expect(factory.isName('Heinz')).toBe(true);
+//        });
+//    });
+});
