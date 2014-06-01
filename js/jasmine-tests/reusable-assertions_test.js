595b7f7bb86b3071c2ca8357426191bf2887c8b7
Dominik Jungowski <Dominik@Jungowski.de>
2014-06-01 22:23:43 +0200

    Move into separate files

--- js/jasmine-tests/reusable-assertions_test.js
+++ js/jasmine-tests/reusable-assertions_test.js
@@ -29,35 +29,4 @@ describe('Reusable Assertions', function() {
             expect(trackingSpy.sendInfo).toHaveBeenCalledWith(123456);
         });
     });
-
-    // Better example
-    describe('loadUser', function() {
-        var trackingSpy = null;
-
-        var assertTrackingCall = function() {
-            expect(trackingSpy.sendInfo).toHaveBeenCalledWith(123456);
-        };
-
-        var setupTrackingSpy = function() {
-            trackingSpy = {
-                sendInfo: function() {}
-            };
-            spyOn(trackingSpy, 'sendInfo');
-        };
-
-        beforeEach(function() {
-            setupTrackingSpy();
-            reusableAssertions.setTracking(trackingSpy);
-        });
-
-        it('sends tracking info when successfully loading a user', function() {
-            reusableAssertions.loadUser({username: 'tobySen'});
-            assertTrackingCall();
-        });
-
-        it('sends tracking info when loading user was not successful', function() {
-            reusableAssertions.loadUser({username: 'foobar'});
-            assertTrackingCall();
-        });
-    });
 });
---
+++ js/jasmine-tests/reusable-assertions_test_better.js
@@ -0,0 +1,38 @@
+describe('Reusable Assertions', function() {
+	var reusableAssertions = null;
+
+	beforeEach(function() {
+		reusableAssertions = new IPC.ReusableAssertions();
+	});
+
+	// Better example
+	describe('loadUser', function() {
+		var trackingSpy = null;
+
+		var assertTrackingCall = function() {
+			expect(trackingSpy.sendInfo).toHaveBeenCalledWith(123456);
+		};
+
+		var setupTrackingSpy = function() {
+			trackingSpy = {
+				sendInfo: function() {}
+			};
+			spyOn(trackingSpy, 'sendInfo');
+		};
+
+		beforeEach(function() {
+			setupTrackingSpy();
+			reusableAssertions.setTracking(trackingSpy);
+		});
+
+		it('sends tracking info when successfully loading a user', function() {
+			reusableAssertions.loadUser({username: 'tobySen'});
+			assertTrackingCall();
+		});
+
+		it('sends tracking info when loading user was not successful', function() {
+			reusableAssertions.loadUser({username: 'foobar'});
+			assertTrackingCall();
+		});
+	});
+});
